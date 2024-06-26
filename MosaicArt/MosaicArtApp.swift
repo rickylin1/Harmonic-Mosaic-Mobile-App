f18a828a3e5bb3be862ec6b11788254f64474db3
rickylin1 <pandanation33@gmail.com>
2024-06-26 15:52:48 -0700

    Initial Commit

---
+++ MosaicArt/MosaicArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  MosaicArtApp.swift
+//  MosaicArt
+//
+//  Created by Fluffy on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MosaicArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
