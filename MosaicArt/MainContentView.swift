4c0e40b147c3d3460ba4335bdbd3fa94be975054
rickylin1 <pandanation33@gmail.com>
2024-07-11 12:40:10 -0700

    Spotify OAuth Authorization Token Flow

---
+++ MosaicArt/MainContentView.swift
@@ -0,0 +1,18 @@
+//
+//  MainContentView.swift
+//  MosaicArt
+//
+//  Created by Fluffy on 7/11/24.
+//
+
+import SwiftUI
+
+struct MainContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainContentView()
+}
